7cedd57e3aec68db1818cecb0a4bb49ae17e57fb
denisestanzione <denisestanzione06@gmail.com>
2025-04-30 19:06:28 +0200

    Aggiunte immagini all'activity finale

--- app/src/main/java/com/example/rollthedice/TerzaActivity.kt
+++ app/src/main/java/com/example/rollthedice/TerzaActivity.kt
@@ -1,6 +1,7 @@
 package com.example.rollthedice
 
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,14 +16,17 @@ class TerzaActivity : AppCompatActivity() {
 
         val TAG = "TerzaActivity"
         val textViewRisultato = findViewById<TextView>(R.id.textViewRisultato)
+        val imageViewIcone = findViewById<ImageView>(R.id.imageViewIcone)
         val mioRandom = intent.getIntExtra("mioRandom", -1)
 
         // Se il numero è dispari
         if (mioRandom % 2 != 0) {
             textViewRisultato.text = "Hai vinto!"
+            imageViewIcone.setImageResource(R.drawable.coppa)
         }
         else {
             textViewRisultato.text = "Hai Perso :("
+            imageViewIcone.setImageResource(R.drawable.faccina)
         }
     }
 }
