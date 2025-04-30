e341ea885f1e34a22775e843ec43268e40d0ff82
denisestanzione <denisestanzione06@gmail.com>
2025-04-30 17:17:46 +0200

    Activity create e dadi importati

---
+++ app/src/main/java/com/example/rollthedice/TerzaActivity.kt
@@ -0,0 +1,16 @@
+package com.example.rollthedice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TerzaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_terza)
+        
+    }
+}
